acf924c6e22b15e6045d1319c51d2eff3266d5e7
Mauricio Filho <ninoheavymetal@hotmail.com>
2023-02-04 21:20:49 -0300

    sc

--- build_publish.sh
+++ build_publish.sh
@@ -1,5 +1,5 @@
-export IMAGENAME=registry.dubaismoke.net.br:5000/api-users:1.0.0
-docker build --network=host -t $IMAGENAME -f ./src/DubaiSmoke.Users.Api/Dockerfile .
-docker rm -f api-users
-docker run -d --name api-users -p 5001:80 -e ASPNETCORE_ENVIRONMENT=prod --restart=always $IMAGENAME
+export IMAGENAME=registry.dubaismoke.net.br:5000/api-locadora:1.0.0
+docker build --network=host -t $IMAGENAME -f ./src/Locadora.WebApp/Dockerfile .
+docker rm -f api-locadora
+docker run -d --name api-locadora -p 5001:80 -e ASPNETCORE_ENVIRONMENT=prod --restart=always $IMAGENAME
 docker rmi -f $IMAGENAME
